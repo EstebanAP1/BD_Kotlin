d04d54f58312597af20b841ca09ea2380d45b83e
EstebanAP1 <estebanap0926@gmail.com>
2024-10-28 22:05:21 -0500

    Fix

--- app/src/main/java/com/example/bd_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/bd_kotlin/MainActivity.kt
@@ -25,14 +25,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalContext
 
 
 class MainActivity : ComponentActivity() {
@@ -44,136 +42,135 @@ class MainActivity : ComponentActivity() {
         dbHelper = DatabaseOpenHelper(this)
 
         setContent {
-            AddUser(dbHelper)
+            UserForm()
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AddUser(dbHelper: DatabaseOpenHelper) {
-    var name by remember { mutableStateOf("") }
-    var lastname by remember { mutableStateOf("") }
-    var age by remember { mutableStateOf("") }
+    @Composable
+    fun UserForm() {
+        var name by remember { mutableStateOf("") }
+        var lastname by remember { mutableStateOf("") }
+        var age by remember { mutableStateOf("") }
 
-    var gender by remember { mutableStateOf("") }
-    var genderOptions = listOf("Male", "Female", "Other")
-    var expanded by remember { mutableStateOf(false) }
+        var gender by remember { mutableStateOf("") }
+        val genderOptions = listOf("Male", "Female", "Other")
+        var expanded by remember { mutableStateOf(false) }
 
-    var phone by remember { mutableStateOf("") }
-    var email by remember { mutableStateOf("") }
+        var phone by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
 
 
 
-    var users by remember { mutableStateOf(dbHelper.getAllUsers()) }
+        var users by remember { mutableStateOf(dbHelper.getAllUsers()) }
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(50.dp),) {
-            TextField(
-                value = name,
-                onValueChange = { name = it },
-                label ={ Text("Name")},
-                modifier = Modifier.padding(2.dp)
-            )
-            TextField(
-                value = lastname,
-                onValueChange = { lastname = it },
-                label ={ Text("Lastname")},
-                modifier = Modifier.padding(2.dp)
-            )
-            TextField(
-                value = age,
-                onValueChange = { age = it },
-                label ={ Text("Age")},
-                modifier = Modifier.padding(2.dp)
-            )
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(50.dp),) {
+                TextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label ={ Text("Name")},
+                    modifier = Modifier.padding(2.dp)
+                )
+                TextField(
+                    value = lastname,
+                    onValueChange = { lastname = it },
+                    label ={ Text("Lastname")},
+                    modifier = Modifier.padding(2.dp)
+                )
+                TextField(
+                    value = age,
+                    onValueChange = { age = it },
+                    label ={ Text("Age")},
+                    modifier = Modifier.padding(2.dp)
+                )
 
-            TextField(
-                value=gender,
-                onValueChange = {},
-                readOnly = true,
-                label = {Text("Gender")},
-                modifier= Modifier.padding(2.dp),
-                trailingIcon = {
-                    IconButton(onClick = { expanded = !expanded } ) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowDropDown,
-                            contentDescription = "Expand Menu")
+                TextField(
+                    value=gender,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = {Text("Gender")},
+                    modifier= Modifier.padding(2.dp),
+                    trailingIcon = {
+                        IconButton(onClick = { expanded = !expanded } ) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowDropDown,
+                                contentDescription = "Expand Menu")
+                        }
+                    }
+                )
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                ) {
+                    genderOptions.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            text= {Text(selectionOption)},
+                            onClick= {
+                                gender = selectionOption
+                                expanded = false
+                            }
+                        )
                     }
                 }
-            )
 
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-            ) {
-                genderOptions.forEach { selectionOption ->
-                    DropdownMenuItem(
-                        text= {Text(selectionOption)},
-                        onClick= {
-                            gender = selectionOption
-                            expanded = false
-                        }
-                    )
+                TextField(
+                    value = phone,
+                    onValueChange = { phone = it },
+                    label ={ Text("Phone")},
+                    modifier = Modifier.padding(2.dp)
+                )
+
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label ={ Text("Email")},
+                    modifier = Modifier.padding(2.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(onClick = {
+                    if(dbHelper.insertUser(name, lastname, age.toIntOrNull() ?: 0, gender, phone, email)) {
+                        Toast.makeText(this@MainActivity, "User added", Toast.LENGTH_LONG).show()
+                        users = dbHelper.getAllUsers()
+
+                        name = ""
+                        lastname = ""
+                        age = ""
+                        gender = ""
+                        phone = ""
+                        email = ""
+                    } else {
+                        Toast.makeText(this@MainActivity, "Error adding user", Toast.LENGTH_LONG).show()
+                    }
+                }) {
+                    Text("Add User")
                 }
-            }
 
-            TextField(
-                value = phone,
-                onValueChange = { phone = it },
-                label ={ Text("Phone")},
-                modifier = Modifier.padding(2.dp)
-            )
+                Spacer(modifier = Modifier.height(16.dp))
 
-            TextField(
-                value = email,
-                onValueChange = { email = it },
-                label ={ Text("Email")},
-                modifier = Modifier.padding(2.dp)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            val context = LocalContext.current
-            Button(onClick = {
-                if(dbHelper.insertUser(name, lastname, age.toIntOrNull() ?: 0, gender, phone, email)) {
-                    Toast.makeText(context, "User added", Toast.LENGTH_LONG).show()
-                    users = dbHelper.getAllUsers()
-
-                    name = ""
-                    lastname = ""
-                    age = ""
-                    gender = ""
-                    phone = ""
-                    email = ""
-                } else {
-                    Toast.makeText(context, "Error adding user", Toast.LENGTH_LONG).show()
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(users) { user ->
+                        UserRow(user)
+                    }
                 }
-            }) {
-                Text("Add User")
+
             }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(users) { user ->
-                    UserRow(user)
-                }
-            }
-
         }
     }
-}
 
-@Composable
-fun UserRow(user: Map<String, Any>) {
-    Column(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
-        Text("Name: ${user["name"]}")
-        Text("Lastname: ${user["lastname"]}")
-        Text("Age: ${user["age"]}")
-        Text("Gender: ${user["gender"]}")
-        Text("Phone: ${user["phone"]}")
-        Text("Email: ${user["email"]}")
-        Spacer(modifier = Modifier.height(8.dp))
+    @Composable
+    fun UserRow(user: Map<String, Any>) {
+        Column(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+            Text("Name: ${user["name"]}")
+            Text("Lastname: ${user["lastname"]}")
+            Text("Age: ${user["age"]}")
+            Text("Gender: ${user["gender"]}")
+            Text("Phone: ${user["phone"]}")
+            Text("Email: ${user["email"]}")
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
+
 }
